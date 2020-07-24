7cc9bcfb4ffd5a46c87dd304d54f4ce64e27f832
ayes-web <52951851+ayes-web@users.noreply.github.com>
2020-07-25 01:50:59 +0300

    Quality of life

--- r2-shows.py
+++ r2-shows.py
@@ -5,11 +5,20 @@ res = requests.get(link)
 res.raise_for_status()
 
 soup = bs4.BeautifulSoup(res.text, "xml")
-musicURL = soup.find_all("enclosure")
-title = soup.find_all("title") #3
-date = soup.find_all("pubDate")
 
+musicURL = soup.find_all("enclosure")
+title = soup.find_all("title")
+date = soup.find_all("pubDate")
 length = soup.find_all("itunes:duration")
+
+#enable if you want to see extra stuff in console
+extra = False
+
+def timeConvert(xd):
+	timestr = xd.text.strip()
+	ftr = [3600,60,1]
+	return str(sum([a*b for a,b in zip(ftr, map(int,timestr.split(':')))]))
+
 
 print("Nothing for current folder.")
 place = input("Folder: ")
@@ -20,20 +29,23 @@ f.write("[playlist] \nNumberOfEntries=" + str(len(musicURL)) + "\n \n")
 i = 1
 while i < len(musicURL):
 
-		timestr = length[i].text.strip()
-		ftr = [3600,60,1]
-		a = str(sum([a*b for a,b in zip(ftr, map(int,timestr.split(':')))]))
+	# converts normal lenght to seconds which the PLS format uses
+	fileLength = timeConvert(length[i])
 
+	#writes all of the info to the file
+	f.write("Title" + str(i) + "=" + title[i].text.strip() + "\n")
+	f.write("Date" + str(i) + "=" + date[i].text.strip() + "\n")
+	f.write("Length" + str(i) + "=" + fileLength + "\n")
+	f.write("File" + str(i) + "=" + musicURL[i].get("url") + "\n \n")
+
+	if extra == True:
 		print(title[i].text.strip())
 		print(musicURL[i].get("url"))
 		print(date[i].text.strip())
-		print(a)
+		print(fileLength)
 		print(" ")
 
-		f.write("Title" + str(i) + "=" + title[i].text.strip() + "\n")
-		f.write("Date" + str(i) + "=" + date[i].text.strip() + "\n")
-		f.write("Length" + str(i) + "=" + a + "\n")
+	i += 1
 
-		f.write("File" + str(i) + "=" + musicURL[i].get("url") + "\n \n")
-		i += 1
+print("Done!")
 f.close()
